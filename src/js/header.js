9753d40c43cce5d88697df77971bd4d0cc736892
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2024-02-06 00:06:13 +0200

    theme dark

--- src/js/header.js
+++ src/js/header.js
@@ -1,20 +1,72 @@
-const body = document.querySelector('body');
-const toggleTheme = document.querySelector('.toggle-theme');
+const themePreference = () => {
+  const hasLocalStorage = localStorage.getItem('theme');
 
-let getMode = localStorage.getItem('mode');
+  let supports = false;
+  let theme = undefined;
 
-if (getMode && getMode === 'dark') {
-  body.classList.add('dark');
-  toggleTheme.classList.add('active');
-}
-toggleTheme.addEventListener('click', () => {
-  body.classList.toggle('dark');
-  if (!body.classList.contains('dark')) {
-    return localStorage.setItem('mode', 'light');
+  if (hasLocalStorage === 'light') {
+    theme = 'light';
   }
-  return localStorage.setItem('mode', 'dark');
-});
+  if (hasLocalStorage === 'dark') {
+    theme = 'dark';
+  }
 
-toggleTheme.addEventListener('click', () =>
-  toggleTheme.classList.toggle('active')
+  if (window.matchMedia(`(prefers-color-scheme: dark)`).matches) {
+    theme = hasLocalStorage ? hasLocalStorage : 'dark';
+    supports = true;
+  }
+  if (window.matchMedia(`(prefers-color-scheme: light)`).matches) {
+    theme = hasLocalStorage ? hasLocalStorage : 'light';
+    supports = true;
+  }
+  if (window.matchMedia(`(prefers-color-scheme: no-preference)`).matches) {
+    theme = hasLocalStorage ? hasLocalStorage : 'none';
+    supports = true;
+  }
+
+  return { supports, theme };
+};
+
+document.addEventListener(
+  'DOMContentLoaded',
+  e => {
+    const userThemePreference = themePreference();
+    const toggle = document.querySelector('.toggle-theme');
+    const html = document.documentElement;
+
+    const setTheme = () => {
+      switch (userThemePreference.theme) {
+        case 'dark':
+          toggle.classList.add('active');
+          html.classList.add('dark');
+          html.classList.remove('light');
+          break;
+        case 'light':
+          toggle.classList.remove('active');
+          html.classList.remove('dark');
+          html.classList.add('light');
+          break;
+      }
+    };
+
+    setTheme();
+
+    toggle.addEventListener(
+      'click',
+      e => {
+        toggle.classList.toggle('active');
+        if (toggle.classList.contains('active')) {
+          html.classList.add('dark');
+          html.classList.remove('light');
+          localStorage.setItem('theme', 'dark');
+        } else {
+          html.classList.remove('dark');
+          html.classList.add('light');
+          localStorage.setItem('theme', 'light');
+        }
+      },
+      false
+    );
+  },
+  false
 );
